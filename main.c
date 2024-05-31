f3f541fa5264cdcaee1a43749d2915c731a04a77
Ilya Ivanov <static.ila@gmail.com>
2024-05-31 18:22:10 +0300

    Simplify font rendering by removing font.c at all

--- main.c
+++ main.c
@@ -4,45 +4,24 @@
 #include "win32.c"
 #include "performance.c"
 #include "format.c"
-#include "font.c"
 
 bool isRunning = 1;
 bool isFullscreen = 0;
-f32 appScale = 1.0f;
+// f32 appScale = 1.0f;
 
-HDC newDc;
+HDC dc;
 BITMAPINFO bitmapInfo;
 MyBitmap canvas;
-HBITMAP myNewBitmap;
+HBITMAP bitmap;
 
+HFONT consolasFont;
+TEXTMETRICA tm;
+
+i32 pagePaddingX = 20;
+i32 pagePaddingY = 15;
 i32 zDelta = 0;
-FontData consolasFont14;
-FontData consolasFontSelected14;
 
-inline void CopyBitmapRectTo(MyBitmap *sourceT, MyBitmap *destination, i32 offsetX, i32 offsetY)
-{
-    // TODO: this should be fixed
-    if (offsetY < 0 || offsetX < 0)
-        return;
-
-    u32 *row = (u32 *)destination->pixels + destination->width * (destination->height - 1) + offsetX - offsetY * destination->width;
-    u32 *source = (u32 *)sourceT->pixels + sourceT->width * (sourceT->height - 1);
-    for (i32 y = 0; y < sourceT->height; y += 1)
-    {
-        u32 *pixel = row;
-        u32 *sourcePixel = source;
-        for (i32 x = 0; x < sourceT->width; x += 1)
-        {
-            // stupid fucking logic needs to replaced
-            if (*sourcePixel != 0xff000000 && (y + offsetY) > 0 && (x + offsetX) > 0 && y + offsetY < destination->height && x + offsetX < destination->width)
-                *pixel = *sourcePixel;
-            sourcePixel += 1;
-            pixel += 1;
-        }
-        source -= sourceT->width;
-        row -= destination->width;
-    }
-}
+i32 cursorPosition = 0;
 
 inline void PaintRect(MyBitmap *destination, i32 offsetX, i32 offsetY, u32 width, u32 height, u32 color)
 {
@@ -52,7 +31,7 @@ inline void PaintRect(MyBitmap *destination, i32 offsetX, i32 offsetY, u32 width
         offsetY = 0;
     }
     // need to check bounds of the screen
-    u32 *row = (u32 *)destination->pixels + offsetX - offsetY * destination->width;
+    u32 *row = (u32 *)destination->pixels + offsetX + offsetY * destination->width;
     for (i32 y = 0; y < height; y += 1)
     {
         u32 *pixel = row;
@@ -64,6 +43,24 @@ inline void PaintRect(MyBitmap *destination, i32 offsetX, i32 offsetY, u32 width
         }
         row += destination->width;
     }
+}
+void InitMyFont()
+{
+    consolasFont = CreateFontA(-MulDiv(14, GetDeviceCaps(dc, LOGPIXELSY), USER_DEFAULT_SCREEN_DPI), 0, 0, 0,
+                               FW_NORMAL, // Weight
+                               0,         // Italic
+                               0,         // Underline
+                               0,         // Strikeout
+                               DEFAULT_CHARSET,
+                               OUT_TT_ONLY_PRECIS,
+                               CLIP_DEFAULT_PRECIS,
+
+                               // I've experimented with the Chrome and it doesn't render LCD quality for fonts above 32px
+                               CLEARTYPE_QUALITY,
+
+                               DEFAULT_PITCH,
+                               "Consolas");
+    GetTextMetricsA(dc, &tm);
 }
 
 LRESULT OnEvent(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
@@ -87,26 +84,24 @@ LRESULT OnEvent(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
         break;
 
     case WM_SIZE:
-        HDC dc = GetDC(window);
-        appScale = (float)GetDeviceCaps(dc, LOGPIXELSY) / (float)USER_DEFAULT_SCREEN_DPI;
+        // appScale = (float)GetDeviceCaps(dc, LOGPIXELSY) / (float)USER_DEFAULT_SCREEN_DPI;
         canvas.width = LOWORD(lParam);
         canvas.height = HIWORD(lParam);
         canvas.bytesPerPixel = 4;
 
-        // if (canvas.pixels)
-        //     DeleteObject(bitmap);
+        if (canvas.pixels)
+            DeleteObject(bitmap);
 
         InitBitmapInfo(&bitmapInfo, canvas.width, canvas.height);
 
-        myNewBitmap = CreateDIBSection(newDc, &bitmapInfo, DIB_RGB_COLORS, &canvas.pixels, 0, 0);
+        bitmap = CreateDIBSection(dc, &bitmapInfo, DIB_RGB_COLORS, &canvas.pixels, 0, 0);
 
-        // fontCanvas = (MyBitmap){.bytesPerPixel = 4, .height = textureSize, .width = textureSize, .pixels = bits};
+        SelectObject(dc, bitmap);
+        SelectObject(dc, consolasFont);
 
-        // TODO: Initialize Arena of screen size and assign proper width and height on resize
+        SetBkMode(dc, TRANSPARENT);
+        SetTextColor(dc, 0xffffff);
 
-        // if (canvas.pixels)
-        //     VirtualFreeMemory(canvas.pixels);
-        // canvas.pixels = VirtualAllocateMemory(sizeof(u32) * canvas.width * canvas.height);
         break;
 
     case WM_KEYDOWN:
@@ -124,26 +119,27 @@ void PrintMetric(char *label, PerfMetric metric)
 {
     u64 time = GetMicrosecondsFor(metric);
 
-    OutputDebugStringA(label);
-    OutputDebugStringA(": ");
     char val[30] = {0};
-    FormatNumber(time, (char *)&val);
-    OutputDebugStringA(val);
-    OutputDebugStringA("us\n");
+    i32 len = FormatNumber(time, (char *)&val);
+
+    TextOutA(dc, canvas.width - 80, canvas.height - tm.tmHeight, val, len);
 }
 
 FileContent file;
+WCHAR *fileContent;
+u32 fileContentSize;
 i32 selectedChar = 2;
 
 void DrawFile()
 {
-    i32 middleY = 20 + zDelta;
-    i32 x = 20;
-    char *lineStart = file.content;
-    char *ch = lineStart;
+    i32 middleY = pagePaddingY + zDelta;
+    i32 x = pagePaddingX;
+
+    WCHAR *lineStart = fileContent;
+    WCHAR *ch = lineStart;
     i32 lineLength = 0;
 
-    for (int i = 0; i < file.size; i++)
+    for (int i = 0; i < fileContentSize; i++)
     {
         if (*ch == '\r')
         {
@@ -151,46 +147,44 @@ void DrawFile()
         else if (*ch == '\n')
         {
 
-            DrawLine(myNewBitmap, newDc, x, middleY, lineStart, lineLength);
+            TextOutW(dc, x, middleY, lineStart, lineLength);
             lineStart = ch + 1;
             lineLength = 0;
-            // x = 20;
-            middleY += 22;
+            middleY += tm.tmHeight;
         }
         else
         {
 
             lineLength++;
-            // MyBitmap *texture = &currentFont->textures[*ch];
-            // CopyBitmapRectTo(texture, &canvas, x, middleY);
-            // x += texture->width + GetKerningValue(*ch, *(ch + 1));
         }
         ch++;
     }
     if (lineLength > 0)
-        DrawLine(myNewBitmap, newDc, x, middleY, lineStart, lineLength);
+        TextOutW(dc, x, middleY, lineStart, lineLength);
 }
 
 void WinMainCRTStartup()
 {
     PreventWindowsDPIScaling();
 
+    dc = CreateCompatibleDC(0);
+
+    InitMyFont(dc);
+
     HWND window = OpenWindow(OnEvent, 0x222222);
     HDC windowDC = GetDC(window);
-
-    newDc = CreateCompatibleDC(0);
 
     MSG msg;
 
     InitPerf();
-    // InitFontSystem();
-    InitMyFont();
 
-    // Arena arena = CreateArena(Megabytes(44));
-    // InitFont(&consolasFont14, FontInfoClearType("Consolas", 14, 0xFFFFFF, 0x00111111), &arena);
-    // InitFont(&consolasFontSelected14, FontInfoClearType("Consolas", 14, 0x000000, 0x00FFFFFF), &arena);
+    // I need to figure what to do with /r symbols on windows if I want to have proper file handling
+    file = ReadMyFileImp("..\\sample.txt");
+    fileContentSize = MultiByteToWideChar(CP_UTF8, 0, file.content, file.size, 0, 0);
 
-    file = ReadMyFileImp("..\\main.c");
+    fileContent = VirtualAllocateMemory(fileContentSize * 2);
+
+    MultiByteToWideChar(CP_UTF8, 0, file.content, file.size, fileContent, fileContentSize);
 
     while (isRunning)
     {
@@ -203,20 +197,21 @@ void WinMainCRTStartup()
         memset(canvas.pixels, 0x11, canvas.bytesPerPixel * canvas.width * canvas.height);
         EndMetric(Memory);
 
-        // PaintRect(&canvas, 0, 0, 10, 10, 0xffffff);
-        // DrawChar(myNewBitmap, newDc);
-
+        SIZE size;
+        GetTextExtentPoint32A(dc, "W", 1, &size);
+        PaintRect(&canvas, pagePaddingX, pagePaddingY, size.cx, tm.tmHeight, 0xaa55aa);
         StartMetric(Draw);
         DrawFile();
         EndMetric(Draw);
 
+        PrintMetric("Memory", Memory);
+
         StartMetric(DiBits);
-        StretchDIBits(windowDC, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height, canvas.pixels, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+        BitBlt(windowDC, 0, 0, canvas.width, canvas.height, dc, 0, 0, SRCCOPY);
         EndMetric(DiBits);
 
-        PrintMetric("Memory", Memory);
-        PrintMetric("Draw", Draw);
-        PrintMetric("DiBits", DiBits);
+        // PrintMetric("Draw", Draw);
+        // PrintMetric("DiBits", DiBits);
 
         Sleep(10); // TODO: proper FPS handling needed, this is just now not to burn CPU
     }
